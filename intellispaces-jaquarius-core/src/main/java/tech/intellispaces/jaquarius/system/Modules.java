2cc0558d57749881a9151aca56b4c0b3aeb34c2f
lavor <lavor.andrey@gmail.com>
2024-12-14 13:10:45 +0600

    Update Modules

--- intellispaces-jaquarius-core/src/main/java/tech/intellispaces/jaquarius/system/Modules.java
+++ intellispaces-jaquarius-core/src/main/java/tech/intellispaces/jaquarius/system/Modules.java
@@ -3,51 +3,57 @@ package tech.intellispaces.jaquarius.system;
 import tech.intellispaces.jaquarius.engine.JaquariusEngines;
 import tech.intellispaces.jaquarius.exception.ConfigurationExceptions;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 /**
  * System modules provider.
  */
 public class Modules {
-  private final static List<Module> MODULES = new ArrayList<>();
+  private final static AtomicReference<Module> MODULE = new AtomicReference<>();
 
   /**
-   * Creates new system module in the application.<p/>
+   * Creates new system module.
    *
    * @param moduleClass the module class.
    * @param args command line arguments.
    * @return the created module.
    */
   public static Module load(Class<?> moduleClass, String[] args) {
-    Module module = JaquariusEngines.get().createModule(List.of(moduleClass), args);
-    MODULES.add(module);
-    return module;
+    return load(() -> JaquariusEngines.get().createModule(List.of(moduleClass), args));
   }
 
   /**
-   * Creates new system module in the application.<p/>
+   * Creates new system module.
    *
    * @param unitClasses unit classes.
    * @return the created module.
    */
   public static Module load(Class<?>... unitClasses) {
-    Module module = JaquariusEngines.get().createModule(Arrays.stream(unitClasses).toList(), new String[0]);
-    MODULES.add(module);
-    return module;
+    return load(() -> JaquariusEngines.get().createModule(Arrays.stream(unitClasses).toList(), new String[0]));
   }
 
   /**
-   * Creates new system module in the application.<p/>
+   * Creates new system module.
    *
    * @param unitClasses unit classes.
    * @param args command line arguments.
    * @return the created module.
    */
   public static Module load(List<Class<?>> unitClasses, String[] args) {
-    Module module = JaquariusEngines.get().createModule(unitClasses, new String[0]);
-    MODULES.add(module);
+    return load(() -> JaquariusEngines.get().createModule(unitClasses, new String[0]));
+  }
+
+  private static Module load(Supplier<Module> moduleSupplier) {
+    if (MODULE.get() != null) {
+      throw ConfigurationExceptions.withMessage("The module has already been uploaded to the application");
+    }
+    Module module = moduleSupplier.get();
+    if (!MODULE.compareAndSet(null, module)) {
+      throw ConfigurationExceptions.withMessage("The module has already been uploaded to the application");
+    };
     return module;
   }
 
@@ -63,7 +69,7 @@ public class Modules {
    */
   public static void unload(Module module) {
     module.stop();
-    MODULES.remove(module);
+    MODULE.set(null);
   }
 
   public static void flare(Class<?>... unitClasses) {
@@ -82,7 +88,7 @@ public class Modules {
   public static Module current() {
     Module module = currentSilently();
     if (module == null) {
-      throw ConfigurationExceptions.withMessage("There are no modules loaded into the application");
+      throw ConfigurationExceptions.withMessage("There are no module loaded into the application");
     }
     return module;
   }
@@ -93,13 +99,7 @@ public class Modules {
    * @return current module or <code>null</code> is module is not loaded into the application.
    */
   public static Module currentSilently() {
-    if (MODULES.isEmpty()) {
-      return null;
-    }
-    if (MODULES.size() > 1) {
-      throw ConfigurationExceptions.withMessage("Multiple modules are loaded into the application");
-    }
-    return MODULES.get(0);
+    return MODULE.get();
   }
 
   private Modules() {}

9eab325115d436909ed1eeb18876656f8402d48f
lavor <lavor.andrey@gmail.com>
2025-02-10 11:01:14 +0600

    Update

--- intellispaces-jaquarius-core/src/main/java/tech/intellispaces/jaquarius/object/reference/ObjectHandleConstants.java
+++ intellispaces-jaquarius-core/src/main/java/tech/intellispaces/jaquarius/object/reference/ObjectHandleConstants.java
@@ -2,5 +2,5 @@ package tech.intellispaces.jaquarius.object.reference;
 
 public interface ObjectHandleConstants {
 
-  String DICTIONARY_HANDLE_CLASSNAME = "tech.intellispaces.ixora.data.association.DictionaryHandle";
+  String DICTIONARY_HANDLE_CLASSNAME = "tech.intellispaces.jaquarius.ixora.data.dictionary.DictionaryHandle";
 }

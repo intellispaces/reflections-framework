9b21270df838df4c603a4e89c5b52423e9e4ae6d
lavor <lavor.andrey@gmail.com>
2025-02-18 11:04:35 +0600

    Update

--- intellispaces-jaquarius-annotationprocessor/src/main/java/tech/intellispaces/jaquarius/annotationprocessor/domain/DomainProcessorFunctions.java
+++ intellispaces-jaquarius-annotationprocessor/src/main/java/tech/intellispaces/jaquarius/annotationprocessor/domain/DomainProcessorFunctions.java
@@ -66,12 +66,6 @@ public interface DomainProcessorFunctions {
   private static void addBasicObjectHandleGenerators(
       CustomType domainType, List<ArtifactGenerator> generators, RoundEnvironment roundEnv
   ) {
-//    String domainName = NameConventionFunctions.convertJaquariusDomainName(domainType.canonicalName());
-//    BasicDomain basicDomain = BasicDomains.active().getByDomainName(domainName);
-//    if (basicDomain != null && basicDomain.delegateClassName() != null) {
-//      return;
-//    }
-
     if (
         AnnotationProcessorFunctions.isAutoGenerationEnabled(domainType, ArtifactTypes.ObjectHandle, roundEnv)
     ) {
